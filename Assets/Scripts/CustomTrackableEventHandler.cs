6b6be5ed0c6a530f9b49e4de1365368fca3abf7c
Luciano Nahuel Vitale <lunavi97@gmail.com>
2019-12-29 04:52:24 -0300

    Modified color of detection image

--- Assets/Scripts/CustomTrackableEventHandler.cs
+++ Assets/Scripts/CustomTrackableEventHandler.cs
@@ -1,16 +1,24 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 
 public class CustomTrackableEventHandler : DefaultTrackableEventHandler
 {
+    public Image detectionImg;
+    [Space]
+    public Color colorFound;
+    public Color colorLost;
+
     protected override void OnTrackingFound()
     {
         base.OnTrackingFound();
         Debug.Log($"<color=green><b> FOUND! </b></color>");
+        detectionImg.color = colorFound;
     }
 
     protected override void OnTrackingLost()
     {
         base.OnTrackingLost();
         Debug.Log($"<color=red><b> LOST! </b></color>");
+        detectionImg.color = colorLost;
     }
 }
